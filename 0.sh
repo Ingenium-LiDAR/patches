e70ec6fc34ba3274218efe90edb797a77113c505
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2026-03-25 20:50:42 -0500

    Append get_new_patches.sh command to .profile

--- 0.sh
+++ 0.sh
@@ -9,3 +9,6 @@ echo "0" >> .patches.txt
 cd ~/Documents/GitHub
 git clone https://github.com/Ingenium-LiDAR/patches.git
 
+
+echo -e "\n./~/Documents/GitHub/patches/get_new_patches.sh #AB batch script to automatically seek and install patches when the user logs in" >> ~/.profile
+
